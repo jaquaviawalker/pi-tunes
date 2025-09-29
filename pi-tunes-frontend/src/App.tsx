65bfd27cdd0bba9d549e282cad9dab024249971a
Jaquavia Walker <jaquavia.walker@copeland.com>
2025-09-28 22:51:18 -0400

    simple frontend interface set up

---
+++ pi-tunes-frontend/src/App.tsx
@@ -0,0 +1,7 @@
+import { HomePage } from '../src/pages/homePage';
+
+function App() {
+  return <HomePage />;
+}
+
+export default App;
---
+++ pi-tunes-frontend/src/pages/HomePage.tsx
@@ -0,0 +1,10 @@
+export const HomePage = () => {
+  return (
+    <>
+      <div className="home">
+        <h1>Welcome to Pi-Tunes!</h1>
+        <p>Please scan an album card to play music</p>
+      </div>
+    </>
+  );
+};
